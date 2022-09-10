d10f49ca01348000e7d6da1713783df3f5a0fb8e
GlanHarith <glan.harithteguh@ui.ac.id>
2022-09-11 00:19:57 +0700

    commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+    'list_item': data_katalog,
+    'nama': 'Glan Harith Teguh',
+    'npm' : '2106752344'
+    }
+    return render(request, "katalog.html",context)
 # TODO: Create your views here.
